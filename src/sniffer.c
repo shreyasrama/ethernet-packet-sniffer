3bacb0370255ac185158d85c92b5533bae00c5a2
shreyasrama <shreyas.rama1@gmail.com>
2015-07-30 14:04:03 +1200

    Gets the packet type and prints it out

--- src/sniffer.c
+++ src/sniffer.c
@@ -6,7 +6,7 @@
 
 int main(int argc, char **argv)
 {
-	struct pcap_pkthdr header; // Header struct for pcap packets
+	struct pcap_pkthdr header; // Header struct for pcap packets. See <pcap.h>
 	const u_char *cur_packet;  // The current packet
 
 	pcap_t *handle;
@@ -19,6 +19,13 @@ int main(int argc, char **argv)
 
 	while (cur_packet = pcap_next(handle, &header)) {
 		printf("Packet length: [%d]\n", header.len);
+
+		// 1. Find packet type contained at the start of the packet.
+		int packet_type = ((int) (cur_packet[12]) << 8) | (int) cur_packet[13];
+		printf("IP type: [%d]\n", packet_type);
+
+		// 2. Get header info: IP version, payload length, source, destination
+		// NOTE: ICMPv6 has header size of 48 bytes, IPv4 (UDP, TCP) has 20 bytes.
 	}
 
 	return 0;
