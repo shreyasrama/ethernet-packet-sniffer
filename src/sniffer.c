56e414039a0602818db52f412153133eaff41e16
shreyasrama <shreyas.rama1@gmail.com>
2015-08-03 21:08:35 +1200

    Using switch&case, IP addresses displaying now

--- src/sniffer.c
+++ src/sniffer.c
@@ -6,6 +6,9 @@
 #include <netinet/ip.h>
 #include <arpa/inet.h>
 
+#define IPv4_TYPE 2048
+#define IPv6_TYPE 34525
+
 int main(int argc, char **argv)
 {
 	struct pcap_pkthdr header; // Header struct for pcap packets. See <pcap.h>
@@ -16,40 +19,57 @@ int main(int argc, char **argv)
 	handle = pcap_open_offline(argv[1], errbuf); // Open pcap file
 	int count = 1; // Initialise a counter of packets
 
+
+	// -----------------------
 	// Error check handle here
+	// -----------------------
 
-	FILE *fp = fopen("result.txt", "w");
 
+	// --------------------------------
+	// Potentially write to a file here
+	// --------------------------------
+
+
+	// Loop through packets
 	while (cur_packet = pcap_next(handle, &header)) {
+		// 0. Standard information about packet,
 		printf("Packet #: %d\n", count);
 		count = count + 1;
-
 		printf("Packet length: %d\n", header.len);
 
 		// 1. Find packet type contained at the start of the packet.
 		int packet_type = ((int) (cur_packet[12]) << 8) | (int) cur_packet[13];
-		if (packet_type == 2048) {
+
+		switch(packet_type)
+		{
+			case IPv4_TYPE :
 			printf("Ether type: IPv4\n");
-		}
-		else {
-			printf("Ether type: IPv6\n");	
+			break;
+
+			case IPv6_TYPE :
+			printf("Ether type: IPv6\n");
+			break;
+
+			default :
+			printf("Unsupported ether type.\n");
+			break;
 		}
 		
-		// 2. Get header info: IP version, payload length, source, destination
+		// 2. Get header info: from/to address, protocol.
 		// NOTE: ICMPv6 has header size of 48 bytes, IPv4 (UDP, TCP) has 20 bytes.
 		cur_packet += 14;
 		struct ip *ip_header = (struct ip*) cur_packet;
 
-		printf("From: ?\n");
-		printf("To: ?\n");
+		printf("From: %s\n", inet_ntoa(ip_header->ip_src));
+		printf("To: %s\n", inet_ntoa(ip_header->ip_dst));
 
 		printf("Protocol: %d\n", ip_header->ip_p);
 
-		// 3. Get packet info: 
-		if (packet_type == 2048) {
+		// 3. Get packet info: src/dst ports, payload size.
+		if (packet_type == IPv4_TYPE) {
 			cur_packet += 20;
 			struct tcphdr *tcp_header = (struct tcphdr*) cur_packet;
-			printf("Src port: %d\n", ntohs(tcp_header->th_sport));
+			printf("Src port: %d\n", ntohs(tcp_header->th_sport)); // ntohs() takes a 16-bit number in TCP/IP network byte order and returns in host byte order
 			printf("Dst port: %d\n", ntohs(tcp_header->th_dport));
 		}
 		else {
@@ -57,8 +77,9 @@ int main(int argc, char **argv)
 			printf("Dst port: IPv6\n");
 		}
 		
-
 		printf("###############################\n");
+
+
 	}
 
 	return 0;
